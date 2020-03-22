d13f9f1236e435b7871c644a1309d50c6d7758a3
Wes Galbraith <galbwe92@gmail.com>
2020-03-22 14:34:56 -0600

    Read STATIC_URL from environment variable

--- backend/stump_backend/stump_backend/stump_backend/settings.py
+++ backend/stump_backend/stump_backend/stump_backend/settings.py
@@ -130,10 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR,
-                           os.path.pardir,
-                           "staticfiles")
+STATIC_URL = os.environ.get('STATIC_URL')
+if STATIC_URL is None:  # defaults for development
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR,
+                               os.path.pardir,
+                               "staticfiles")
+
 
 # Localhost React server
 CORS_ORIGIN_WHITELIST = (

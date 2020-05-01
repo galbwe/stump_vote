a506f7be3dd54aeaf212b73a2bcd6b645bc76b76
Wes Galbraith <galbwe92@gmail.com>
2020-05-01 00:01:36 -0600

    Format with black

--- backend/stump_backend/stump_backend/api/proxies/civicengine.py
+++ backend/stump_backend/stump_backend/api/proxies/civicengine.py
@@ -3,65 +3,77 @@ import requests
 
 
 class ApiCaller(object):
-    '''
+    """
     High-level wrapper to the Python requests library
-    '''
+    """
 
     def __init__(self, *args, **kwargs):
-        if not hasattr(self, 'base_url'):
+        if not hasattr(self, "base_url"):
             raise ValueError("Missing base_url")
-        self.base_url = getattr(self, 'base_url')
-        self.timeout = kwargs.get('timeout', 10)
+        self.base_url = getattr(self, "base_url")
+        self.timeout = kwargs.get("timeout", 10)
 
     def _get_headers(self):
-        '''
+        """
         Returns necessary header needed by the API.
         Currently only the authentication header is set.
-        '''
+        """
         headers = dict()
-        if hasattr(self, 'authentication_header'):
-            auth = getattr(self, 'authentication_header')
+        if hasattr(self, "authentication_header"):
+            auth = getattr(self, "authentication_header")
             for key in auth:
                 headers[key] = auth[key]
         return headers
 
     def _validate_optional_params(self, resource, optional_params):
-        '''
+        """
         If optional query parameters are passed to the API call, this will
         check that they are contained in a whitelist.
-        '''
-        if not hasattr(self, 'resource_optional_params_map'):
+        """
+        if not hasattr(self, "resource_optional_params_map"):
             return
-        resource_optional_params_map = getattr(self, 'resource_optional_params_map')
+        resource_optional_params_map = getattr(self, "resource_optional_params_map")
         if resource not in resource_optional_params_map:
             return
         optional_params_keys = resource_optional_params_map[resource]
         for param in optional_params:
             if param not in optional_params_keys:
-                raise ValueError("Optional parameter '{param}' not valid for resource '{resource}'".format(param=param, resource=resource))
+                raise ValueError(
+                    "Optional parameter '{param}' not valid for resource '{resource}'".format(
+                        param=param, resource=resource
+                    )
+                )
 
     def _get_request(self, full_url, optional_params=None):
         headers = self._get_headers()
         # Makes the actual call to the API
         # TODO: Add error handling, timeouts, and request throttling
-        response = requests.get(full_url, headers=headers, params=optional_params, timeout=self.timeout)
+        response = requests.get(
+            full_url, headers=headers, params=optional_params, timeout=self.timeout
+        )
         return response
 
     def fetch(self, resource, *args, **kwargs):
         # Setup optional query parameters
         optional_params = None
-        if 'optional_params' in kwargs and kwargs['optional_params'] is not None:
-            self._validate_optional_params(resource, kwargs['optional_params'])
-            optional_params = kwargs['optional_params']
+        if "optional_params" in kwargs and kwargs["optional_params"] is not None:
+            self._validate_optional_params(resource, kwargs["optional_params"])
+            optional_params = kwargs["optional_params"]
 
         # The positional arguments after the resource, e.g. /candidates/1
-        positional_args = '/'.join([str(a) for a in args])
-        if not positional_args == '':
+        positional_args = "/".join([str(a) for a in args])
+        if not positional_args == "":
             # Positional arguements added after slash
-            full_url = "{base_url}/{resource}/{positional_args}".format(base_url=self.base_url, resource=resource, positional_args=positional_args)
+            full_url = "{base_url}/{resource}/{positional_args}".format(
+                base_url=self.base_url,
+                resource=resource,
+                positional_args=positional_args,
+            )
         else:
             # No postional arguments are use for list queries
-            full_url = "{base_url}/{resource}".format(base_url=self.base_url, resource=resource)
+            full_url = "{base_url}/{resource}".format(
+                base_url=self.base_url, resource=resource
+            )
 
         # Make the request
         # TODO: cache the respone, but before implementing caching, check with BallotReady to make sure this complies with our contractual agreement.
@@ -73,121 +85,168 @@ class ApiCaller(object):
         else:
             # Something went wrong
             success = False
-            json = {'status_code': response.status_code, 'service_response': response.json()}
+            json = {
+                "status_code": response.status_code,
+                "service_response": response.json(),
+            }
 
         # return a tuple with the success flag and the JSON payload
         return success, json
 
 
 class CivicEngineApi(ApiCaller):
-    '''
+    """
     Class that encapsulates the calls to BallotRead CivicEngine API.
     See https://developers.civicengine.com/docs/api
-    '''
+    """
 
     # Top-level endpoint
     base_url = "https://api.civicengine.com"
 
     # Setup the API key here
-    authentication_header = {'x-api-key': os.getenv('CIVICENGINE_API_KEY')}
+    authentication_header = {"x-api-key": os.getenv("CIVICENGINE_API_KEY")}
 
     # Resources that are appended to the base_url
-    CANDIDATE_RESOURCE = 'candidate'
-    DISTRICTS_RESOURCE = 'districts'
-    ELECTIONS_RESOURCE = 'elections'
-    MEASURES_RESOURCE = 'measures'
-    NORMALIZED_POSITIONS_RESOURCE = 'normalized-positions'
-    OFFICE_HOLDERS_RESOURCE = 'office-holders'
-    POLLING_PLACES_RESOURCE = 'polling_places' 
-    POSITIONS_RESOURCE = 'positions' 
+    CANDIDATE_RESOURCE = "candidate"
+    DISTRICTS_RESOURCE = "districts"
+    ELECTIONS_RESOURCE = "elections"
+    MEASURES_RESOURCE = "measures"
+    NORMALIZED_POSITIONS_RESOURCE = "normalized-positions"
+    OFFICE_HOLDERS_RESOURCE = "office-holders"
+    POLLING_PLACES_RESOURCE = "polling_places"
+    POSITIONS_RESOURCE = "positions"
     # This whitelist is used for validating the optional parameters
     resource_optional_params_map = {
-        CANDIDATE_RESOURCE: ('election_id', 'tenant_id'),
-        DISTRICTS_RESOURCE: ('address', 'lat', 'long'),
-        ELECTIONS_RESOURCE: ('end_at', 'latitude', 'longitude', 'start_at', 'state', 'updated_since'),
-        MEASURES_RESOURCE: ('address', 'lat', 'lng', 'election_id', 'election_date'),  
-        NORMALIZED_POSITIONS_RESOURCE: tuple(), 
-        OFFICE_HOLDERS_RESOURCE: ('address', 'lat', 'lng'),
-        POLLING_PLACES_RESOURCE: ('election_id', 'latitude', 'longitude', 'street_number', 'pre_directional', 'street_name', 'street_suffix', 'post_directional', 'city', 'state', 'zipcode'), 
-        POSITIONS_RESOURCE: ('address', 'county', 'election_date', 'election_id', 'exclude_statewide', 'include_candidates', 'include_endorsements', 'include_office_holders', 'include_uncertified_candidates', 'include_volunteer_urls', 'lat', 'level', 'lon', 'search_radius', 'state', 'tier', 'year'), 
+        CANDIDATE_RESOURCE: ("election_id", "tenant_id"),
+        DISTRICTS_RESOURCE: ("address", "lat", "long"),
+        ELECTIONS_RESOURCE: (
+            "end_at",
+            "latitude",
+            "longitude",
+            "start_at",
+            "state",
+            "updated_since",
+        ),
+        MEASURES_RESOURCE: ("address", "lat", "lng", "election_id", "election_date"),
+        NORMALIZED_POSITIONS_RESOURCE: tuple(),
+        OFFICE_HOLDERS_RESOURCE: ("address", "lat", "lng"),
+        POLLING_PLACES_RESOURCE: (
+            "election_id",
+            "latitude",
+            "longitude",
+            "street_number",
+            "pre_directional",
+            "street_name",
+            "street_suffix",
+            "post_directional",
+            "city",
+            "state",
+            "zipcode",
+        ),
+        POSITIONS_RESOURCE: (
+            "address",
+            "county",
+            "election_date",
+            "election_id",
+            "exclude_statewide",
+            "include_candidates",
+            "include_endorsements",
+            "include_office_holders",
+            "include_uncertified_candidates",
+            "include_volunteer_urls",
+            "lat",
+            "level",
+            "lon",
+            "search_radius",
+            "state",
+            "tier",
+            "year",
+        ),
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def get_candidate(self, candidate_id, optional_params=None):
-        '''
+        """
         GET /candidate/:id
         See https://developers.civicengine.com/docs/api/candidates/by-id
-        '''
-        result = self.fetch(self.CANDIDATE_RESOURCE, candidate_id, optional_params=optional_params)
+        """
+        result = self.fetch(
+            self.CANDIDATE_RESOURCE, candidate_id, optional_params=optional_params
+        )
 
         # return the tuple (success, json)
         return result
 
     def get_districts(self, optional_params=None):
-        '''
+        """
         GET /districts
         See https://developers.civicengine.com/docs/api/districts/list
         Note: optional_params are not really optiomal. Either address or lat/lon is required.
-        '''
+        """
         result = self.fetch(self.DISTRICTS_RESOURCE, optional_params=optional_params)
 
         # return the tuple (success, json)
         return result
 
     def get_elections(self, optional_params=None):
-        '''
+        """
         GET /elections
         See https://developers.civicengine.com/docs/api/elections/list
-        '''
+        """
         result = self.fetch(self.ELECTIONS_RESOURCE, optional_params=optional_params)
         # return the tuple (success, json)
         return result
 
     def get_measures(self, optional_params=None):
-        '''
+        """
         GET /elections
         See https://developers.civicengine.com/docs/api/measures/list
-        '''
+        """
         result = self.fetch(self.MEASURES_RESOURCE, optional_params=optional_params)
         # return the tuple (success, json)
         return result
 
     def get_normalized_positions(self, optional_params=None):
-        '''
+        """
         GET /elections
         See https://developers.civicengine.com/docs/api/normalized-positions/list
 
-        '''
-        result = self.fetch(self.NORMALIZED_POSITIONS_RESOURCE, optional_params=optional_params)
+        """
+        result = self.fetch(
+            self.NORMALIZED_POSITIONS_RESOURCE, optional_params=optional_params
+        )
         # return the tuple (success, json)
         return result
 
     def get_office_holders(self, optional_params=None):
-        '''
+        """
         GET /elections
         See https://developers.civicengine.com/docs/api/office-holders/list
-        '''
-        result = self.fetch(self.OFFICE_HOLDERS_RESOURCE, optional_params=optional_params)
+        """
+        result = self.fetch(
+            self.OFFICE_HOLDERS_RESOURCE, optional_params=optional_params
+        )
         # return the tuple (success, json)
         return result
 
     def get_polling_places(self, optional_params=None):
-        '''
+        """
         GET /elections
         See https://developers.civicengine.com/docs/api/polling_places/list
-        '''
-        result = self.fetch(self.POLLING_PLACES_RESOURCE, optional_params=optional_params)
+        """
+        result = self.fetch(
+            self.POLLING_PLACES_RESOURCE, optional_params=optional_params
+        )
         # return the tuple (success, json)
         return result
 
     def get_positions(self, optional_params=None):
-        '''
+        """
         GET /elections
         See https://developers.civicengine.com/docs/api/positions/list
-        '''
+        """
         result = self.fetch(self.POSITIONS_RESOURCE, optional_params=optional_params)
         # return the tuple (success, json)
         return result
-
--- backend/stump_backend/stump_backend/api/tests/functional/test_proxies_functional.py
+++ backend/stump_backend/stump_backend/api/tests/functional/test_proxies_functional.py
@@ -11,57 +11,65 @@ from ...proxies.civicengine import CivicEngineApi
 
 class TestCivicEngineDistrictsEndpointSuccess(SimpleTestCase):
     def setUp(self):
-        self.resource = 'districts'
+        self.resource = "districts"
         self.saved_json = self._get_saved_response()
         self.args = []
         self.optional_params = {
-            'address': '12017+W+Alameda+Pkwy+Lakewood+CO+80228',
-        } 
+            "address": "12017+W+Alameda+Pkwy+Lakewood+CO+80228",
+        }
         self.api = CivicEngineApi(timeout=5)
 
     def test_success_response(self):
         try:
-            success, response_json = self.api.get_districts(optional_params=self.optional_params)
+            success, response_json = self.api.get_districts(
+                optional_params=self.optional_params
+            )
             assert success, (
-                f'Error calling get districts',
+                f"Error calling get districts",
                 f'\nURL\n\t{self.api.base_url}/districts?address={self.optional_params["address"]}',
                 f'\nresponse_code\n\t{response_json["status_code"]}.',
-                f'\nresponse_body\n\t{json.dumps(response_json["service_response"], indent=2, default=str)}')
+                f'\nresponse_body\n\t{json.dumps(response_json["service_response"], indent=2, default=str)}',
+            )
         except Timeout:
             assert False, (
-                f'Timeout calling get districts after {self.api.timeout} seconds.',
+                f"Timeout calling get districts after {self.api.timeout} seconds.",
                 f'\nURL\n\t{self.api.base_url}/districts?address={self.optional_params["address"]}',
             )
         self._check_json(response_json)
 
     def _get_saved_response(self):
-            path_to_json = os.path.abspath(os.path.join(
+        path_to_json = os.path.abspath(
+            os.path.join(
                 os.path.dirname(__file__),
                 os.path.pardir,
-                'data',
-                f'{self.resource}.json'
-            ))
-            with open(path_to_json) as f:
-                saved_json = json.loads(f.read())
-                return saved_json
+                "data",
+                f"{self.resource}.json",
+            )
+        )
+        with open(path_to_json) as f:
+            saved_json = json.loads(f.read())
+            return saved_json
 
     def _check_json(self, response_json):
-        saved_json = deepcopy(self.saved_json)  # so that we do not mess up the instances state in other tests when deleting timestamp keys
-        self.assertIn('timestamp', response_json)
+        saved_json = deepcopy(
+            self.saved_json
+        )  # so that we do not mess up the instances state in other tests when deleting timestamp keys
+        self.assertIn("timestamp", response_json)
         # example timestamp: 2020-05-01T02:58:11.283642
-        self.assertRegexpMatches(response_json['timestamp'], r'20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}')
-        del saved_json['timestamp']
-        del response_json['timestamp']
+        self.assertRegexpMatches(
+            response_json["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
+        )
+        del saved_json["timestamp"]
+        del response_json["timestamp"]
         self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
 
-    
-class TestLiveCivicEngineApi(SimpleTestCase):
 
+class TestLiveCivicEngineApi(SimpleTestCase):
     def setUp(self):
-        self.data_folder = ''
+        self.data_folder = ""
         self.api = CivicEngineApi(timeout=5)
         # check that responses from the api have been saved to the data folder
-        
+
         # if they have not, set a flag to save data to the folder when it is retrieved
 
     def tearDown(self):
@@ -69,54 +77,57 @@ class TestLiveCivicEngineApi(SimpleTestCase):
 
     def test_get_candidates_success_response(self):
         candidate_id = 1
-        optional_params = {
-            'election_id': 1 
-        }
+        optional_params = {"election_id": 1}
         try:
-            success, json_ = self.api.get_candidate(candidate_id, optional_params=optional_params)
+            success, json_ = self.api.get_candidate(
+                candidate_id, optional_params=optional_params
+            )
             assert success, (
-                f'Error calling get candidate',
+                f"Error calling get candidate",
                 f'\nURL\n\t{self.api.base_url}/candidate/1?election_id={optional_params["election_id"]}',
                 f'\nresponse_code\n\t{json_["status_code"]}.',
-                f'\nresponse_body\n\t{json.dumps(json_["service_response"], indent=2, default=str)}')
+                f'\nresponse_body\n\t{json.dumps(json_["service_response"], indent=2, default=str)}',
+            )
         except Timeout:
             assert False, (
-                f'Timeout calling get districts after {self.api.timeout} seconds.',
+                f"Timeout calling get districts after {self.api.timeout} seconds.",
                 f'\nURL\n\t{self.api.base_url}/candidate/1?election_id={optional_params["election_id"]}',
             )
         # check that the data matches what is saved on disk
-        saved_json = self._get_saved_response('candidate')
+        saved_json = self._get_saved_response("candidate")
         assert saved_json is not None
         self.assertJSONEqual(json.dumps(json_), json.dumps(saved_json))
 
-
     def test_get_elections(self):
         optional_params = {
-            'latitude': 39.7416945,
-            'longitude': -104.9883106,
-            'start_at': '2018-01-01',
-            'end_at': '2019-01-01',
+            "latitude": 39.7416945,
+            "longitude": -104.9883106,
+            "start_at": "2018-01-01",
+            "end_at": "2019-01-01",
         }
         try:
             success, json_ = self.api.get_elections(optional_params=optional_params)
             assert success, (
-                f'Error calling get elections',
+                f"Error calling get elections",
                 f'\nURL\n\t{self.api.base_url}/elections?{"&".join(str(p) + "=" + str(v) for (p, v) in optional_params.items())}'
                 f'\nresponse_code\n\t{json_["status_code"]}.',
-                f'\nresponse_body\n\t{json.dumps(json_["service_response"], indent=2, default=str)}')
+                f'\nresponse_body\n\t{json.dumps(json_["service_response"], indent=2, default=str)}',
+            )
         except Timeout:
             assert False, (
-                f'Timeout calling get elections after {self.api.timeout} seconds.',
-                f'\nURL\n\t{self.api.base_url}/elections?{"&".join(str(p) + "=" + str(v) for (p, v) in optional_params.items())}'
+                f"Timeout calling get elections after {self.api.timeout} seconds.",
+                f'\nURL\n\t{self.api.base_url}/elections?{"&".join(str(p) + "=" + str(v) for (p, v) in optional_params.items())}',
             )
         # check that the data matches what is saved on disk
-        saved_json = self._get_saved_response('elections')
+        saved_json = self._get_saved_response("elections")
         assert saved_json is not None
-        self.assertIn('timestamp', json_)
+        self.assertIn("timestamp", json_)
         # example timestamp: 2020-05-01T02:58:11.283642
-        self.assertRegexpMatches(json_['timestamp'], r'20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}')
-        del json_['timestamp']
-        del saved_json['timestamp']
+        self.assertRegexpMatches(
+            json_["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
+        )
+        del json_["timestamp"]
+        del saved_json["timestamp"]
         self.assertJSONEqual(json.dumps(json_), json.dumps(saved_json))
 
     def _save_response_to_data_directory(self, response):
@@ -126,12 +137,11 @@ class TestLiveCivicEngineApi(SimpleTestCase):
         pass
 
     def _get_saved_response(self, resource):
-        path_to_json = os.path.abspath(os.path.join(
-            os.path.dirname(__file__),
-            os.path.pardir,
-            'data',
-            f'{resource}.json'
-        ))
+        path_to_json = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), os.path.pardir, "data", f"{resource}.json"
+            )
+        )
         with open(path_to_json) as f:
             saved_json = json.loads(f.read())
             return saved_json

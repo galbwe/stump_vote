099cf441a4015b1f3e377453e31e14de772256b7
Wes Galbraith <galbwe92@gmail.com>
2020-05-03 15:11:56 -0600

    Add functional test for each civic engine endpoint

--- backend/stump_backend/stump_backend/api/tests/functional/test_proxies_functional.py
+++ backend/stump_backend/stump_backend/api/tests/functional/test_proxies_functional.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 from copy import deepcopy
@@ -14,6 +15,7 @@ class AbstractBaseMixin(object):
     def setUp(self):
         # self.resource = "districts"
         self.saved_json = self._get_saved_response()
+        assert self.saved_json, f"Error loading saved civic engine response for resource {self.resource}."
         # self.args = []
         # self.optional_params = {
             # "address": "12017+W+Alameda+Pkwy+Lakewood+CO+80228",
@@ -24,6 +26,7 @@ class AbstractBaseMixin(object):
     def test_success_response(self):
         try:
             success, response_json = self.api_method(
+                *self.args,
                 optional_params=self.optional_params
             )
             assert success, (
@@ -58,6 +61,24 @@ class AbstractBaseMixin(object):
         return '?' + "&".join(f'{k}={v}' for (k, v) in self.optional_params.items())
 
 
+class TestCivicEngineGetCandidatesSuccess(AbstractBaseMixin, SimpleTestCase):
+
+    resource = "candidate"
+
+    def setUp(self):
+        super().setUp()
+        self.args = [
+            1,  # candidate_id
+        ]
+        self.optional_params = {
+            "election_id": 1,
+        }
+        self.api_method = self.api.get_candidate
+
+    def _check_json(self, response_json):
+        self.assertJSONEqual(json.dumps(self.saved_json), response_json)
+
+
 class TestCivicEngineGetDistrictsSuccess(AbstractBaseMixin, SimpleTestCase):
 
     resource = "districts" 
@@ -84,88 +105,155 @@ class TestCivicEngineGetDistrictsSuccess(AbstractBaseMixin, SimpleTestCase):
         self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
 
 
-@skip
-class TestLiveCivicEngineApi(SimpleTestCase):
+class TestCivicEngineGetElectionsSuccess(AbstractBaseMixin, SimpleTestCase):
+
+    resource = "elections" 
+
     def setUp(self):
-        self.data_folder = ""
-        self.api = CivicEngineApi(timeout=5)
-        # check that responses from the api have been saved to the data folder
-
-        # if they have not, set a flag to save data to the folder when it is retrieved
-
-    def tearDown(self):
-        pass
-
-    def test_get_candidates_success_response(self):
-        candidate_id = 1
-        optional_params = {"election_id": 1}
-        try:
-            success, json_ = self.api.get_candidate(
-                candidate_id, optional_params=optional_params
-            )
-            assert success, (
-                f"Error calling get candidate",
-                f'\nURL\n\t{self.api.base_url}/candidate/1?election_id={optional_params["election_id"]}',
-                f'\nresponse_code\n\t{json_["status_code"]}.',
-                f'\nresponse_body\n\t{json.dumps(json_["service_response"], indent=2, default=str)}',
-            )
-        except Timeout:
-            assert False, (
-                f"Timeout calling get districts after {self.api.timeout} seconds.",
-                f'\nURL\n\t{self.api.base_url}/candidate/1?election_id={optional_params["election_id"]}',
-            )
-        # check that the data matches what is saved on disk
-        saved_json = self._get_saved_response("candidate")
-        assert saved_json is not None
-        self.assertJSONEqual(json.dumps(json_), json.dumps(saved_json))
-
-    def test_get_elections(self):
-        optional_params = {
+        super().setUp()
+        self.args = []
+        self.optional_params = {
             "latitude": 39.7416945,
             "longitude": -104.9883106,
             "start_at": "2018-01-01",
             "end_at": "2019-01-01",
         }
-        try:
-            success, json_ = self.api.get_elections(optional_params=optional_params)
-            assert success, (
-                f"Error calling get elections",
-                f'\nURL\n\t{self.api.base_url}/elections?{"&".join(str(p) + "=" + str(v) for (p, v) in optional_params.items())}'
-                f'\nresponse_code\n\t{json_["status_code"]}.',
-                f'\nresponse_body\n\t{json.dumps(json_["service_response"], indent=2, default=str)}',
-            )
-        except Timeout:
-            assert False, (
-                f"Timeout calling get elections after {self.api.timeout} seconds.",
-                f'\nURL\n\t{self.api.base_url}/elections?{"&".join(str(p) + "=" + str(v) for (p, v) in optional_params.items())}',
-            )
-        # check that the data matches what is saved on disk
-        saved_json = self._get_saved_response("elections")
-        assert saved_json is not None
-        self.assertIn("timestamp", json_)
+        self.api_method = self.api.get_elections
+
+    def _check_json(self, response_json):
+        saved_json = deepcopy(
+            self.saved_json
+        )  # so that we do not mess up the instance's state in other tests when deleting timestamp keys
+        self.assertIn("timestamp", response_json)
         # example timestamp: 2020-05-01T02:58:11.283642
         self.assertRegexpMatches(
-            json_["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
+            response_json["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
         )
-        del json_["timestamp"]
         del saved_json["timestamp"]
-        self.assertJSONEqual(json.dumps(json_), json.dumps(saved_json))
+        del response_json["timestamp"]
+        self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
 
-    def _save_response_to_data_directory(self, response):
-        pass
 
-    def _check_data_directory_for_response(self, response):
-        pass
+class TestCivicEngineGetMeasuresSuccess(AbstractBaseMixin, SimpleTestCase):
 
-    def _get_saved_response(self, resource):
-        path_to_json = os.path.abspath(
-            os.path.join(
-                os.path.dirname(__file__), os.path.pardir, "data", f"{resource}.json"
-            )
+    resource = "measures" 
+
+    def setUp(self):
+        super().setUp()
+        self.args = []
+        self.optional_params = {
+            "address": "200+16th+St+Mall+Denver+CO+80202",
+            "election_id": 392,  # id for CO primary election
+        }
+        self.api_method = self.api.get_measures
+
+    def _check_json(self, response_json):
+        saved_json = deepcopy(
+            self.saved_json
+        )  # so that we do not mess up the instance's state in other tests when deleting timestamp keys
+        self.assertIn("timestamp", response_json)
+        # example timestamp: 2020-05-01T02:58:11.283642
+        self.assertRegexpMatches(
+            response_json["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
         )
-        with open(path_to_json) as f:
-            saved_json = json.loads(f.read())
-            return saved_json
+        del saved_json["timestamp"]
+        del response_json["timestamp"]
+        self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
 
-    def _check_api_response_matches_saved_response(self, resources):
-        pass
+
+class TestCivicEngineGetNormalizedPositionsSuccess(AbstractBaseMixin, SimpleTestCase):
+
+    resource = "normalized-positions" 
+
+    def setUp(self):
+        super().setUp()
+        self.args = []
+        self.optional_params = dict()
+        self.api_method = self.api.get_normalized_positions
+
+    def _check_json(self, response_json):
+        self.assertJSONEqual(json.dumps(response_json), json.dumps(self.saved_json))
+
+
+
+class TestCivicEngineGetsOfficeHoldersSuccess(AbstractBaseMixin, SimpleTestCase):
+
+    resource = "office-holders" 
+
+    def setUp(self):
+        super().setUp()
+        self.args = []
+        self.optional_params = {
+            "address": "200+16th+St+Mall+Denver+CO+80202"
+        }
+        self.api_method = self.api.get_office_holders
+
+    def _check_json(self, response_json):
+        saved_json = deepcopy(
+            self.saved_json
+        )  # so that we do not mess up the instance's state in other tests when deleting timestamp keys
+        self.assertIn("timestamp", response_json)
+        # example timestamp: 2020-05-01T02:58:11.283642
+        self.assertRegexpMatches(
+            response_json["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
+        )
+        del saved_json["timestamp"]
+        del response_json["timestamp"]
+        self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
+
+
+class TestCivicEngineGetPollingPlacesSuccess(AbstractBaseMixin, SimpleTestCase):
+
+    resource = "polling_places"  # the underscore is not a typo
+
+    def setUp(self):
+        super().setUp()
+        self.args = []
+        self.optional_params = { 
+            "election_id": 392, # CO general election
+            "latitude": 39.7416945,
+            "longitude": -104.9883106,  
+        }
+        self.api_method = self.api.get_polling_places
+
+    def _check_json(self, response_json):
+        saved_json = deepcopy(
+            self.saved_json
+        )  # so that we do not mess up the instance's state in other tests when deleting timestamp keys
+        self.assertIn("timestamp", response_json)
+        # example timestamp: 2020-05-01T02:58:11.283642
+        self.assertRegexpMatches(
+            response_json["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
+        )
+        del saved_json["timestamp"]
+        del response_json["timestamp"]
+        self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
+
+
+class TestCivicEngineGetPositionsSuccess(AbstractBaseMixin, SimpleTestCase):
+
+    resource = "positions" 
+
+    def setUp(self):
+        super().setUp()
+        self.args = []
+        self.optional_params = { 
+            "address": "200+16th+St+Mall+Denver+CO+80202",
+            "include_candidates": 1,
+            "election_date": '2016-11-08',
+        }
+        self.api_method = self.api.get_positions
+
+    def _check_json(self, response_json):
+        saved_json = deepcopy(
+            self.saved_json
+        )  # so that we do not mess up the instance's state in other tests when deleting timestamp keys
+        self.assertIn("timestamp", response_json)
+        # example timestamp: 2020-05-01T02:58:11.283642
+        self.assertRegexpMatches(
+            response_json["timestamp"], r"20\d\d-\d{2}-\d{2}T\d{2}:\d{2}.\d{2}\.\d{6}"
+        )
+        del saved_json["timestamp"]
+        del response_json["timestamp"]
+        saved_json["address"] = saved_json["address"].replace(' ', '+')
+        self.assertJSONEqual(json.dumps(response_json), json.dumps(saved_json))
